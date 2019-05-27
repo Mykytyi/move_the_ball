9444476bccb2309fbb28b30ad461a73460f079f7
Nikita <cherevaty.nik@gmail.com>
2019-05-27 22:11:38 +0300

    'versalite_code'

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,29 @@
 let ball = document.getElementById('ball');
 let field = document.getElementById('field');
 
+let imgWidth = ball.clientWidth;
+let imgHeight = ball.clientHeight;
+let borderWidth = 10;
+let fieldHeight = 300;
+let fieldWidth = 250;
+
+field.style.width = `${fieldWidth}px`;
+field.style.height = `${fieldHeight}px`;
+field.style.borderWidth = `${borderWidth}px`
+
 field.addEventListener('click', function (event) {
   let x = event.clientX;
   let y = event.clientY;
 
-  if (x < 20) {
-    x = 20;
-  } else if (x > 180) {
-    x = 180;
+  if (x < (imgWidth / 2)) {
+    x = imgWidth / 2;
+  } else if (x > fieldWidth - imgWidth / 2) {
+    x = fieldWidth - imgWidth / 2;
   }
-  if (y < 20) {
-    y = 20;
-  } else if (y > 130) {
-    y = 130;
+  if (y < (imgHeight / 2)) {
+    y = imgHeight / 2;
+  } else if (y > fieldHeight - imgHeight / 2) {
+    y = fieldHeight - imgHeight / 2;
   }
 
   ball.style.top = y.toString() + 'px';
